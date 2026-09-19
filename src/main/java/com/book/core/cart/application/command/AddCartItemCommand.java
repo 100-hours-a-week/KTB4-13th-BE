7ca74495a9a112945c99705cee663cbc9a989eb7
023-dev <rhdgmltkd88@gmail.com>
2026-09-19 18:57:03 +0900

    refactor: 장바구니 코드 포맷과 import 정리(#10)

--- src/main/java/com/book/core/cart/application/command/AddCartItemCommand.java
+++ src/main/java/com/book/core/cart/application/command/AddCartItemCommand.java
@@ -1,7 +1,3 @@
 package com.book.core.cart.application.command;
 
-public record AddCartItemCommand(
-    Long userId,
-    Long productId,
-    Integer quantity
-) {}
+public record AddCartItemCommand(Long userId, Long productId, Integer quantity) {}

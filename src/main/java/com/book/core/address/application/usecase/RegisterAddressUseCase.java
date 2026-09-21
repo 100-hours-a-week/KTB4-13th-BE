13ebbf2232445f235b11276e4358b1967e9108e4
023-dev <rhdgmltkd88@gmail.com>
2026-09-21 09:06:19 +0900

    docs: 주소지 등록 흐름 주석 명확화(#26)

--- src/main/java/com/book/core/address/application/usecase/RegisterAddressUseCase.java
+++ src/main/java/com/book/core/address/application/usecase/RegisterAddressUseCase.java
@@ -18,6 +18,7 @@ public class RegisterAddressUseCase {
 
     @Transactional
     public void execute(final RegisterAddressCommand command) {
+        // 활성 주소지가 최대 개수에 도달하면 예외 발생
         final int activeAddressCount = addressRepository.countActiveByUserId(command.userId());
         if (activeAddressCount >= MAX_ACTIVE_ADDRESS_COUNT) {
             throw new CoreException(ErrorCode.ADDRESS_LIMIT_EXCEEDED);
@@ -25,24 +26,30 @@ public class RegisterAddressUseCase {
 
         final Address address = Address.register(
                 command.userId(), command.label(), command.postalCode(), command.address(), command.detailAddress());
+        // 완전히 동일한 주소지(주소, 상세주소, 별칭)가 이미 존재하면 예외 발생
         if (addressRepository.existsActiveDuplicateAddress(command.userId(), address)) {
             throw new CoreException(ErrorCode.DUPLICATE_ADDRESS);
         }
 
+        // 첫 번째 주소지인지 확인
         final boolean firstAddress = activeAddressCount == 0;
-        Address currentDefault = null;
+        Address currentDefaultAddress = null;
+        // 기본 배송지로 요청한 경우 기존 기본 배송지를 조회
         if (command.isDefaultAddress() && !firstAddress) {
-            currentDefault = addressRepository
+            currentDefaultAddress = addressRepository
                     .findActiveDefaultByUserId(command.userId())
                     .orElse(null);
         }
 
+        // 첫 번째 주소지이거나 기본 배송지로 요청한 경우 기본 배송지로 설정
         if (firstAddress || command.isDefaultAddress()) {
             address.setDefaultAddress();
         }
+        // 주소지 저장
         addressRepository.save(address);
-        if (currentDefault != null) {
-            currentDefault.unsetDefaultAddress();
+        // 신규 주소지 저장에 성공한 경우 기존 기본 배송지를 해제
+        if (currentDefaultAddress != null) {
+            currentDefaultAddress.unsetDefaultAddress();
         }
     }
 }
